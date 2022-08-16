73dffbf5569483cad2a5e6b78803f795a0bb1709
meliodev <salimlyoussi1@gmail.com>
2022-08-16 16:31:57 +0100

    Batterie deploiement part 2

--- functions/functions/db/projects/onWrite.js
+++ functions/functions/db/projects/onWrite.js
@@ -45,13 +45,13 @@ exports.onWriteProject = functions.firestore
 
         //2. Notifications & Emails
         //Set receivers
-        const admins = await getUsersByRole('Admin')
+        //const admins = await getUsersByRole('Admin')
         const respTechs = await getUsersByRole('Responsable technique')
         const directeurComs = await getUsersByRole('Directeur commercial')
         let receivers = [after.comContact, after.client] //#task: add intervenant if he exists
         if (after.techContact.id !== "")
             receivers.push(after.techContact)
-        receivers = receivers.concat(admins, respTechs, directeurComs)
+        receivers = receivers.concat(respTechs, directeurComs)
         //receivers = initReceivers(receivers, after.editedBy.id)
         receivers = project_messageReceivers_filter(receivers, event, before, after)
 
